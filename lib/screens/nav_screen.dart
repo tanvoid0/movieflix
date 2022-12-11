189a91f19e8144ffb961773b7f4f0ba36f0eef9b
Tanveer Hoque <tanvoid0@gmail.com>
2022-12-11 21:19:06 +0000

    wip - push before going to germany

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -35,26 +35,26 @@ class _NavScreenState extends State<NavScreen> {
         create: (_) => AppBarCubit(),
         child: _screens[_currentIndex],
       ),
-      bottomNavigationBar: !Responsive.isDesktop(context) ?
-      BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.black,
-        items: _icons
-            .map((title, icon) => MapEntry(
-                title,
-                BottomNavigationBarItem(
-                  icon: Icon(icon, size: 30.0),
-                  title: Text(title),
-                )))
-            .values
-            .toList(),
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.white,
-        selectedFontSize: 11.0,
-        unselectedItemColor: Colors.grey,
-        unselectedFontSize: 11.0,
-        onTap: (index) => setState(() => _currentIndex = index),
-      )
+      bottomNavigationBar: !Responsive.isDesktop(context)
+          ? BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.black,
+              items: _icons
+                  .map((title, icon) => MapEntry(
+                      title,
+                      BottomNavigationBarItem(
+                        icon: Icon(icon, size: 30.0),
+                        label: title,
+                      )))
+                  .values
+                  .toList(),
+              currentIndex: _currentIndex,
+              selectedItemColor: Colors.white,
+              selectedFontSize: 11.0,
+              unselectedItemColor: Colors.grey,
+              unselectedFontSize: 11.0,
+              onTap: (index) => setState(() => _currentIndex = index),
+            )
           : null,
     );
   }
